d00acfa7dea8418cd1335a9df7b14b9ba2e4cd46
IlmanNafi04 <ilmannafi04@gmail.com>
2025-02-01 13:28:14 +0700

    menambahkan route /dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('login');
+});
+
+Route::post('/login',[UserController::class,'login']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
 });
